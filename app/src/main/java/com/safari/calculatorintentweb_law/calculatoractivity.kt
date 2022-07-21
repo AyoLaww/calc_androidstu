0530f4707a798cf25865a3f51d4a9958c34891ee
AyoLaww <lawrenceonsongo@gmail.com>
2022-07-21 08:55:01 +0300

    initial commit

--- app/src/main/java/com/safari/calculatorintentweb_law/calculatoractivity.kt
+++ app/src/main/java/com/safari/calculatorintentweb_law/calculatoractivity.kt
@@ -2,10 +2,85 @@ package com.safari.calculatorintentweb_law
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class calculatoractivity : AppCompatActivity() {
+
+    lateinit var btn_add:Button
+    lateinit var btn_sub:Button
+    lateinit var btn_divi:Button
+    lateinit var btn_multi:Button
+
+
+    lateinit var edt_fnum:EditText
+    lateinit var edt_snum:EditText
+
+
+    lateinit var txt_ans:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculatoractivity)
+
+        edt_fnum = findViewById(R.id.edt_fnum)
+        edt_snum = findViewById(R.id.edt_snum)
+        txt_ans = findViewById(R.id.txt_answer)
+
+        btn_add = findViewById(R.id.btn_add)
+        btn_add!!.setOnClickListener {
+            var firstnum = edt_fnum?.text.toString()
+            var secondnum = edt_snum?.text.toString()
+            if (firstnum.isEmpty() && secondnum.isEmpty()){
+                txt_ans.text = "Please input some numbers!"
+            }else{
+                var ans = firstnum.toDouble() + secondnum.toDouble()
+
+                txt_ans.text = ans.toString()
+            }
+        }
+
+        btn_sub = findViewById(R.id.btn_minus)
+        btn_sub!!.setOnClickListener {
+            var firstnum = edt_fnum?.text.toString()
+            var secondnum = edt_snum?.text.toString()
+            if (firstnum.isEmpty() && secondnum.isEmpty()){
+                txt_ans.text = "Please input some numbers!"
+            }else{
+                var ans = firstnum.toDouble() - secondnum.toDouble()
+
+                txt_ans.text = ans.toString()
+            }
+
+        }
+
+        btn_divi = findViewById(R.id.btn_div)
+        btn_divi!!.setOnClickListener {
+            var firstnum = edt_fnum?.text.toString()
+            var secondnum = edt_snum?.text.toString()
+            if (firstnum.isEmpty() && secondnum.isEmpty()){
+                txt_ans.text = "Please input some numbers!"
+            }else{
+                var ans = firstnum.toDouble() / secondnum.toDouble()
+
+                txt_ans.text = ans.toString()
+            }
+
+        }
+
+        btn_multi = findViewById(R.id.btn_mult)
+        btn_multi!!.setOnClickListener {
+            var firstnum = edt_fnum?.text.toString()
+            var secondnum = edt_snum?.text.toString()
+            if (firstnum.isEmpty() && secondnum.isEmpty()){
+                txt_ans.text = "Please input some numbers!"
+            }else{
+                var ans = firstnum.toDouble() * secondnum.toDouble()
+
+                txt_ans.text = ans.toString()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/safari/calculatorintentweb_law/intentactivity.kt
+++ app/src/main/java/com/safari/calculatorintentweb_law/intentactivity.kt
@@ -1,11 +1,106 @@
 package com.safari.calculatorintentweb_law
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class intentactivity : AppCompatActivity() {
+     var buttonSms: Button ?= null
+     var buttonEmail: Button ?= null
+    var buttonShare: Button ?= null
+    var buttonCamera: Button ?= null
+     var buttonStk: Button ?= null
+     var buttonCall: Button ?= null
+     var buttonDial: Button ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intentactivity)
+
+        setContentView(R.layout.activity_main)
+        buttonSms = findViewById(R.id.btn_sms)
+        buttonEmail = findViewById(R.id.btn_email)
+        buttonShare = findViewById(R.id.btn_share)
+        buttonCamera = findViewById(R.id.btn_camera)
+        buttonStk = findViewById(R.id.btn_stk)
+        buttonCall = findViewById(R.id.btn_call)
+        buttonDial = findViewById(R.id.btn_dial)
+
+        buttonSms!!.setOnClickListener {
+            val uri = Uri.parse("smsto:0715950987")
+
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+            intent.putExtra("Wassup nigga", "Pop out at yo party")
+
+            startActivity(intent)
+        }
+
+        buttonEmail!!.setOnClickListener {
+            val emailIntent =
+                Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "abc@gmail.com", null))
+
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Body")
+
+            startActivity(Intent.createChooser(emailIntent, "Send email..."))
+        }
+
+        buttonShare!!.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Hey, download this app!")
+            startActivity(shareIntent)
+        }
+
+        buttonCamera!!.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            startActivityForResult(takePictureIntent, 1)
+
+        }
+
+        buttonStk!!.setOnClickListener {
+            val simToolKitLaunchIntent =
+                applicationContext.packageManager.getLaunchIntentForPackage("com.android.stk")
+
+            simToolKitLaunchIntent?.let { startActivity(it) }
+
+        }
+
+        buttonDial!!.setOnClickListener {
+            val phone = "+34666777888"
+
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+
+            startActivity(intent)
+        }
+
+        buttonCall!!.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+918511812660"))
+
+            if (ContextCompat.checkSelfPermission(
+                    this@intentactivity,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@intentactivity,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    1
+                )
+            } else {
+                startActivity(intent)
+            }
+
+        }
     }
 }
